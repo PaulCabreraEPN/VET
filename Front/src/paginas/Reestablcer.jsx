2197578b2b83d943c6b9c8b166e393acba1aeeb2
PaulCabreraEPN <paul.cabrera@epn.edu.ec>
2025-01-20 15:58:43 -0500

    recuperar contraseña

--- Front/src/paginas/Reestablcer.jsx
+++ Front/src/paginas/Reestablcer.jsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import {ToastContainer, toast} from 'react-toastify'
+import logoDog from '../assets/doglost.jpg'
 
 export default function Reestablcer() {
     //Paso 1
@@ -32,7 +33,7 @@ export default function Reestablcer() {
             toast.success(respuesta.data.msg)
         } catch (error) {
             console.log(error);
-            toast.error(error.response.data.msg)
+            
         }
     }
 
@@ -45,7 +46,7 @@ export default function Reestablcer() {
             toast.success(respuesta.data.msg)
         } catch (error) {
             console.log(error);
-            toast.error(error.respuesta.data.msg)
+
         }
     }
 
@@ -56,22 +57,31 @@ export default function Reestablcer() {
   return (
     <div>
         <ToastContainer />
-        {
-            tokenback &&
-            <form action="" onSubmit={handleSubmit}>
-                <label htmlFor="">Contraseña</label>
-                <input type="password"
-                 name='password'
-                 value={form.password || ""}
-                 onChange={handleChange} />
-                <label htmlFor="">Nueva Contraseña</label>
-                <input type="password" 
-                name='confirmpassword'
-                value={form.confirmpassword}
-                onChange={handleChange}/>
-                <button>Enviar</button>
-            </form>
-        }
+        <h1 className="text-3xl font-semibold mb-2 text-center uppercase  text-gray-500">Welcome again</h1>
+      <small className="text-gray-400 block my-4 text-sm">Please enter your details</small>
+      <img className="object-cover h-80 w-80 rounded-full border-4 border-solid border-slate-600" src={logoDog} alt="image description" />
+      {tokenback &&
+          <form className='w-full' onSubmit={handleSubmit}>
+              <div className="mb-1">
+                  <label className="mb-2 block text-sm font-semibold">Password</label>
+                  <input type="password" placeholder="Enter your password" className="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-1.5 text-gray-500"
+                      value={form.password || ""}
+                      name='password'
+                      onChange={handleChange}
+                  />
+                  <label className="mb-2 block text-sm font-semibold">Confirm password</label>
+                  <input type="password" placeholder="Repeat your password" className="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-1.5 text-gray-500"
+                      value={form.confirmpassword || ""}
+                      name='confirmpassword'
+                      onChange={handleChange}
+                  />
+              </div>
+              <div className="mb-3">
+                  <button className="bg-gray-600 text-slate-300 border py-2 w-full rounded-xl mt-5 hover:scale-105 duration-300 hover:bg-gray-900 hover:text-white">Send
+                  </button>
+              </div>
+          </form>
+      }
         
     </div>
   )
